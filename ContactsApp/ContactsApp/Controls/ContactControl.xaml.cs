79f6778e206ebd6c9c519e1f0db219bbefa654cf
Kevan Rahav <kevan.or@gmail.com>
2018-12-12 13:05:21 -0700

    Added User Controls for Contact binding (including redefinition)

--- ContactsApp/ContactsApp/Controls/ContactControl.xaml.cs
+++ ContactsApp/ContactsApp/Controls/ContactControl.xaml.cs
@@ -21,9 +21,9 @@ namespace ContactsApp.Controls
     /// </summary>
     public partial class ContactControl : UserControl
     {
-        private Contact contact;
+        /*private Contact contact;
 
-        public Contact MyProperty
+        public Contact Contact
         {
             get { return contact; }
             set
@@ -34,8 +34,33 @@ namespace ContactsApp.Controls
                 emailTextBlock.Text = contact.Email;
 
             }
+        }*/
+
+
+
+        public Contact Contact
+        {
+            get { return (Contact)GetValue(ContactProperty); }
+            set { SetValue(ContactProperty, value); }
         }
 
+
+        //Provide a way to compute the value of a property based on the value of different things or different inputs
+        // Using a DependencyProperty as the backing store for Contact.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ContactProperty =
+            DependencyProperty.Register("Contact", typeof(Contact), typeof(ContactControl), new PropertyMetadata(new Contact(),SetText));
+
+        private static void SetText(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ContactControl control = d as ContactControl;
+
+            if(control != null)
+            {
+                control.nameTextBlock.Text = ((Contact)e.NewValue).Name;
+                control.phoneTextBlock.Text = ((Contact)e.NewValue).PhoneNum;
+                control.emailTextBlock.Text = ((Contact)e.NewValue).Email;
+            }
+        }
 
         public ContactControl()
         {
